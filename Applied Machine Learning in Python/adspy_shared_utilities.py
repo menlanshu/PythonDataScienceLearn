b0e93be50c2017b84543f70bb139dd0814ed76fb
fulong <menlanshu@hotmail.com>
2020-11-18 17:37:46 +0800

    Weekly 1 class finished, udpate file

--- Applied Machine Learning in Python/adspy_shared_utilities.py
+++ Applied Machine Learning in Python/adspy_shared_utilities.py
@@ -1,4 +1,5 @@
-# version 1.1
+# as_matrix# version 1.1
+# as_matrix change to to_numpy
 
 import numpy
 import pandas as pd
@@ -169,8 +170,8 @@ def plot_class_regions_for_classifier(clf, X, y, X_test=None, y_test=None, title
     plt.show()
     
 def plot_fruit_knn(X, y, n_neighbors, weights):
-    X_mat = X[['height', 'width']].as_matrix()
-    y_mat = y.as_matrix()
+    X_mat = X[['height', 'width']].values
+    y_mat = y.values
 
     # Create color maps
     cmap_light = ListedColormap(['#FFAAAA', '#AAFFAA', '#AAAAFF','#AFAFAF'])
